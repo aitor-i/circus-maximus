1d7247529375e20477aaa0e5f7b2f5fb98b354a9
Aitor Ibarra <ibarraaitor@protonmail.com>
2025-02-11 22:40:52 +0100

    feat(Events): add mongo config

---
+++ server-actions/mongo/mongo.ts
@@ -0,0 +1,20 @@
+"use server";
+import { MongoClient, MongoClientOptions, Db } from "mongodb";
+
+type DbName = "circus-maximus";
+type Collection = "events";
+
+const mongoClientOptions: MongoClientOptions = {};
+
+const uri = process.env.MONGO_CONNECTION_STRING;
+
+export async function getMongoClinet() {
+  return new MongoClient(uri!, mongoClientOptions);
+}
+
+export async function getPointer(collection: Collection) {
+  const dbName: DbName = "circus-maximus";
+  const mongoClient = await getMongoClinet();
+  const database: Db = mongoClient.db(dbName);
+  return database.collection(collection);
+}
