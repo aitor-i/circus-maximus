f968524bef55f4e8596884962b2c44703a5aec27
Aitor Ibarra <ibarraaitor@protonmail.com>
2025-02-12 21:29:54 +0100

    refactor(Event): event card on event section

--- components/EventSection/EventSection.tsx
+++ components/EventSection/EventSection.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { getDictionary } from "@/translations/getDictionary";
 import { getEvents } from "@/server-actions/events/events";
 import { getEventIcon, getEventImage, getEventTitle } from "@/lib/events";
+import EventCard from "../EventCard/EventCard";
 
 export default async function EventSection() {
   const dict = await getDictionary("en");
@@ -24,35 +25,7 @@ export default async function EventSection() {
         </h2>
         <div className="grid md:grid-cols-3 gap-8">
           {events.map((event) => (
-            <Link href={`/events/${event._id}`} key={event._id}>
-              <Card key={event.title} className="overflow-hidden group">
-                <div className="relative h-48 overflow-hidden">
-                  <img
-                    src={getEventImage(event.eventType)}
-                    alt={event.title}
-                    className="w-full h-full object-cover transition-transform group-hover:scale-105"
-                  />
-                </div>
-                <div className="p-6">
-                  <div className="flex items-center gap-2 mb-4">
-                    {getEventIcon(event.icon)}
-                    <h3 className="text-xl font-semibold">
-                      {getEventTitle(event)}
-                    </h3>
-                  </div>
-                  <p className="text-muted-foreground mb-4">
-                    {event.description}
-                  </p>
-                  <div className="flex justify-between items-center">
-                    <span className="font-bold">${event.price}</span>
-                    <span className="font-bold">
-                      {new Date(event.date).toLocaleDateString()}
-                    </span>
-                    <Button>{dict.eventSection.bookNow}</Button>
-                  </div>
-                </div>
-              </Card>
-            </Link>
+            <EventCard event={event} />
           ))}
         </div>
       </div>
