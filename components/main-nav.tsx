15ec5302ac74acfc701efbfe9aadd38b58bc72e4
Aitor Ibarra <ibarraaitor@protonmail.com>
2025-02-19 22:33:31 +0100

    feat(Cart): display if items

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -3,8 +3,13 @@
 import Link from "next/link";
 import { Sword, ShoppingBag, User } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { useShoppingBag } from "@/stores/shopingBagContext";
 
 export function MainNav() {
+  const { items } = useShoppingBag();
+  const itemsCount = items.length;
+  const isCartEmpty = itemsCount === 0;
+
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="flex h-16 items-center px-4">
@@ -19,8 +24,14 @@ export function MainNav() {
           >
             Events
           </Link>
-          <Link href="/shop" className="text-sm font-medium hover:text-primary">
+          <Link
+            href="/cart"
+            className=" text-sm font-medium hover:text-primary"
+          >
             <ShoppingBag className="w-5 h-5" />
+            {!isCartEmpty && (
+              <span className="block ml-2 absolute translate-x-2 -translate-y-1 h-2 w-2 bg-red-500 rounded-full"></span>
+            )}
           </Link>
           <Button variant="outline" size="sm" asChild>
             <Link href="/login">
